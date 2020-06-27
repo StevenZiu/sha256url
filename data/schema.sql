c4909cab8944a9180cc4d0b8814ec5de18323404
stevenzhao <zhwdyx1201@gmail.com>
2020-06-27 12:03:47 +0800

    change schema indexing && update github ci

--- data/schema.sql
+++ data/schema.sql
@@ -3,9 +3,9 @@ use sha256url;
 
 create table if not exists hashlinks (
   url_id int not null auto_increment,
-  original_url varchar(2000) not null,
+  original_url varchar(1000) not null,
   long_hash varchar(400) not null,
   short_hash varchar(10) not null,
   primary key(url_id),
-  unique(url_id, original_url, long_hash) 
+  unique(original_url) 
 )
